d3a666316bba06dd9b2348df5043408499be0c05
vishalthapa07 <vishalqubits@gmail.com>
2024-10-03 16:55:10 +0545

    create tools and experience section in form

--- src/app/forms/questions-form .tsx
+++ src/app/forms/questions-form .tsx
@@ -12,6 +12,20 @@ export type TInputs = {
   linkedin: string;
   phone_number: string;
   skills: { skillName: string }[];
+  tools: { toolName: string }[];
+  experiences: {
+    position: string;
+    companyName: string;
+    joinedDate: string;
+    leftDate: string;
+    shortDescription: string;
+  }[];
+  education: {
+    course: string;
+    institute: string;
+    startDate: string;
+    endDate: string;
+  }[];
 };
 
 type Props = {
@@ -20,6 +34,35 @@ type Props = {
 
 export default function QuestionsForm({ onDataChange }: Props) {
   const [skills, setSkills] = useState([{ skillName: "" }]);
+  const [tools, setTools] = useState([{ toolName: "" }]);
+  const [experiences, setExperiences] = useState([
+    {
+      position: "",
+      companyName: "",
+      joinedDate: "",
+      leftDate: "",
+      shortDescription: "",
+    },
+  ]);
+
+  const [position, setPosition] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [joinedDate, setJoinedDate] = useState("");
+  const [leftDate, setLeftDate] = useState("");
+
+  const [educations, setEducations] = useState([
+    {
+      course: "",
+      institute: "",
+      startDate: "",
+      endDate: "",
+    },
+  ]);
+
+  const [position, setPosition] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [joinedDate, setJoinedDate] = useState("");
+  const [leftDate, setLeftDate] = useState("");
 
   const {
     register,
@@ -31,6 +74,8 @@ export default function QuestionsForm({ onDataChange }: Props) {
 
   const onSubmit: SubmitHandler<TInputs> = (data) => {
     data.skills = skills;
+    data.tools = tools;
+    data.experiences = experiences;
     console.log(data);
     onDataChange(data);
     resetForm();
@@ -40,6 +85,16 @@ export default function QuestionsForm({ onDataChange }: Props) {
   const resetForm = () => {
     reset();
     setSkills([{ skillName: "" }]); // Reset skills
+    setTools([{ toolName: "" }]);
+    setExperiences([
+      {
+        position: "",
+        companyName: "",
+        joinedDate: "",
+        leftDate: "",
+        shortDescription: "",
+      },
+    ]);
   };
 
   const addSkill = () => {
@@ -52,12 +107,52 @@ export default function QuestionsForm({ onDataChange }: Props) {
     setSkills(newSkills);
   };
 
+  const addTool = () => {
+    setTools([...tools, { toolName: "" }]);
+  };
+
+  const handleToolChange = (index: number, value: string) => {
+    const newTools = [...tools];
+    newTools[index].toolName = value;
+    setTools(newTools);
+  };
+
+  const addExperience = () => {
+    setExperiences([
+      ...experiences,
+      {
+        position: "",
+        companyName: "",
+        joinedDate: "",
+        leftDate: "",
+        shortDescription: "",
+      },
+    ]);
+  };
+
+  const handleExperienceChange = (
+    index: number,
+    position: string,
+    companyName: string,
+    joinedDate: string,
+    leftDate: string,
+    shortDescription: string
+  ) => {
+    const newExperiences = [...experiences];
+    newExperiences[index].position = position;
+    newExperiences[index].companyName = companyName;
+    newExperiences[index].joinedDate = joinedDate;
+    newExperiences[index].leftDate = leftDate;
+    newExperiences[index].shortDescription = shortDescription;
+    setExperiences(newExperiences);
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="text-black p-6 rounded-lg shadow-md">
         <div className="mb-4">
-          <label htmlFor="name">
-            What is your name? <span className="text-red-500">*</span>
+          <label htmlFor="name" className="font-bold text-lg">
+            Name <span className="text-red-500">*</span>
           </label>
           <br />
           <input
@@ -72,8 +167,8 @@ export default function QuestionsForm({ onDataChange }: Props) {
           )}
         </div>
         <div className="mb-4">
-          <label htmlFor="profession">
-            What is your profession? <span className="text-red-500">*</span>
+          <label htmlFor="profession" className="font-bold text-lg">
+            Profession <span className="text-red-500">*</span>
           </label>
           <input
             {...register("profession", { required: true })}
@@ -86,8 +181,8 @@ export default function QuestionsForm({ onDataChange }: Props) {
           )}
         </div>
         <div className="mb-4">
-          <label htmlFor="short_intro">
-            Can you provide a short description about yourself?{" "}
+          <label htmlFor="short_intro" className="font-bold text-lg">
+            Short description about yourself{" "}
             <span className="text-red-500">*</span>
           </label>
           <textarea
@@ -101,8 +196,8 @@ export default function QuestionsForm({ onDataChange }: Props) {
           )}
         </div>
         <div className="mb-4">
-          <label htmlFor="email">
-            What is your email address? <span className="text-red-500">*</span>
+          <label htmlFor="email" className="font-bold text-lg">
+            Email address <span className="text-red-500">*</span>
           </label>
           <input
             {...register("email", {
@@ -117,9 +212,8 @@ export default function QuestionsForm({ onDataChange }: Props) {
           )}
         </div>
         <div className="mb-4">
-          <label htmlFor="linkedin">
-            Please share your LinkedIn profile details.{" "}
-            <span className="text-red-500">*</span>
+          <label htmlFor="linkedin" className="font-bold text-lg">
+            LinkedIn profile <span className="text-red-500">*</span>
           </label>
           <input
             {...register("linkedin", { required: true })}
@@ -131,8 +225,8 @@ export default function QuestionsForm({ onDataChange }: Props) {
           )}
         </div>
         <div className="mb-4">
-          <label htmlFor="phone_number">
-            What is your phone number? <span className="text-red-500">*</span>
+          <label htmlFor="phone_number" className="font-bold text-lg">
+            Phone number <span className="text-red-500">*</span>
           </label>
           <input
             {...register("phone_number", { required: true })}
@@ -145,8 +239,8 @@ export default function QuestionsForm({ onDataChange }: Props) {
           )}
         </div>
         <div className="shadow-md rounded-md p-4 mb-4">
-          <label htmlFor="skills">
-            Please share your skills? <span className="text-red-500">*</span>
+          <label htmlFor="skills" className="font-bold text-lg">
+            Skills <span className="text-red-500">*</span>
           </label>
           {skills.map((skill, index) => (
             <div key={index} className="mb-1">
@@ -164,8 +258,118 @@ export default function QuestionsForm({ onDataChange }: Props) {
           ))}
           <div className="flex justify-center">
             <IoMdAddCircle
-              className="text-red-500 hover:opacity-70 my-2 cursor-pointer"
+              className="text-red-600 hover:opacity-70 my-2 cursor-pointer"
               onClick={addSkill}
+            />
+          </div>{" "}
+        </div>
+        <div className="shadow-md rounded-md p-4 mb-4">
+          <label htmlFor="tools" className="font-bold text-lg">
+            Tools and Technology <span className="text-red-500">*</span>
+          </label>
+          {tools.map((tool, index) => (
+            <div key={index} className="mb-1">
+              <input
+                value={tool.toolName}
+                onChange={(e) => handleToolChange(index, e.target.value)}
+                placeholder="Eg: React"
+                id="tools"
+                className="border border-gray-400 px-2 py-2 my-1 rounded-md w-full"
+              />
+              {tool.toolName === "" && (
+                <span className="text-red-500 text-lg">*Tool is required</span>
+              )}
+            </div>
+          ))}
+          <div className="flex justify-center">
+            <IoMdAddCircle
+              className="text-red-500 hover:opacity-70 my-2 cursor-pointer"
+              onClick={addTool}
+            />
+          </div>{" "}
+        </div>
+        <div className="shadow-md rounded-md p-4 mb-4">
+          <label htmlFor="experiences" className="font-bold text-lg">
+            Experience <span className="text-red-500">*</span>
+          </label>
+          {experiences.map((experience, index) => (
+            <div key={index} className="my-4 p-6 rounded-lg shadow-md">
+              <input
+                value={experience.position}
+                onChange={(e) => setPosition(e.target.value)}
+                placeholder="Enter your position"
+                id="experiences"
+                className="border border-gray-400 px-2 py-2 my-1 rounded-md w-full"
+              />
+              {experience.position === "" && (
+                <span className="text-red-500 text-lg">
+                  *Position is required
+                </span>
+              )}
+              <input
+                value={experience.companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
+                placeholder="Enter your company name"
+                id="experiences"
+                className="border border-gray-400 px-2 py-2 my-1 rounded-md w-full"
+              />
+              {experience.companyName === "" && (
+                <span className="text-red-500 text-lg">
+                  *Company is required
+                </span>
+              )}
+              <input
+                value={experience.joinedDate}
+                onChange={(e) => setJoinedDate(e.target.value)}
+                placeholder="Enter your joined date. Eg: 3rd October, 2024"
+                id="experiences"
+                className="border border-gray-400 px-2 py-2 my-1 rounded-md w-full"
+              />
+              {experience.joinedDate === "" && (
+                <span className="text-red-500 text-lg">
+                  *Joined date is required
+                </span>
+              )}
+              <input
+                value={experience.leftDate}
+                onChange={(e) => setLeftDate(e.target.value)}
+                placeholder="Enter your left date. Eg: 3rd October, 2025"
+                id="experiences"
+                className="border border-gray-400 px-2 py-2 my-1 rounded-md w-full"
+              />
+              {experience.leftDate === "" && (
+                <span className="text-red-500 text-lg">
+                  *Left date is required
+                </span>
+              )}
+              <textarea
+                value={experience.shortDescription}
+                rows={3}
+                onChange={(e) =>
+                  handleExperienceChange(
+                    index,
+                    position,
+                    companyName,
+                    joinedDate,
+                    leftDate,
+                    e.target.value
+                  )
+                }
+                placeholder="Enter your job short description"
+                id="experiences"
+                className="border border-gray-400 px-2 py-2 my-1 rounded-md w-full"
+              />
+              {experience.shortDescription === "" && (
+                <span className="text-red-500 text-lg">
+                  *Job short description is required
+                </span>
+              )}
+            </div>
+          ))}
+          <div className="flex justify-center">
+            <IoMdAddCircle
+              className="text-red-500 hover:opacity-70 my-2 cursor-pointer"
+              onClick={addExperience}
             />
           </div>{" "}
         </div>
