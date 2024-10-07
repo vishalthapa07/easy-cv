fe54dca4993f5bb5dffbc372064a6d0ea66a2735
vishalthapa07 <vishalqubits@gmail.com>
2024-10-07 15:10:22 +0545

    fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar";
 
 export const metadata: Metadata = {
   title: "Easy CV",
